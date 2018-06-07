7cba0409a87ab0bd2fe2fea9fa3963f81592fc41
Squishy123 <christian.gnaw@gmail.com>
2018-06-07 00:20:15 -0400

    TODO fix 3 episode saving glitch

--- schemas/episodeSchema.js
+++ schemas/episodeSchema.js
@@ -4,6 +4,8 @@ const Source = require('./sourceSchema.js');
 let EpisodeSchema = new mongoose.Schema({
     id: Number,
     title: String, 
+    english: String,
+    duration: Number,
     sources: [{player: String, url: String, quality: String}],
 })
 
